45907fb89ffb44d00f04d0b0a596d9766272b45a
Gilbert Kibet <kibetgilly354@gmail.com>
2022-01-30 09:23:10 +0300

    Work on Team
    Developed inputs forms for collecting team documents and sharing team documents and also announcements

--- src/app/administrator/teamannouncement/teamannouncement.component.ts
+++ src/app/administrator/teamannouncement/teamannouncement.component.ts
@@ -3,13 +3,61 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-teamannouncement',
   templateUrl: './teamannouncement.component.html',
-  styleUrls: ['./teamannouncement.component.css']
+  styleUrls: ['./teamannouncement.component.css'],
 })
 export class TeamannouncementComponent implements OnInit {
+  announcements = [
+    {
+      title: 'Announcements',
+      date: '29/01/2000',
+      published: 'this is published',
+      attachment: 'attached',
+      notify: 'i have notify',
+      content: 'content',
+    },
+    {
+      title: 'Announcements',
+      date: '29/01/2000',
+      published: 'this is published',
+      attachment: 'attached',
+      notify: 'i have notify',
+      content: 'content',
+    },
+    {
+      title: 'Announcements',
+      date: '29/01/2000',
+      published: 'this is published',
+      attachment: 'attached',
+      notify: 'i have notify',
+      content: 'content',
+    },
+    {
+      title: 'Announcements',
+      date: '29/01/2000',
+      published: 'this is published',
+      attachment: 'attached',
+      notify: 'i have notify',
+      content: 'content',
+    },
+    {
+      title: 'Announcements',
+      date: '29/01/2000',
+      published: 'this is published',
+      attachment: 'attached',
+      notify: 'i have notify',
+      content: 'content',
+    },
+    {
+      title: 'Announcements',
+      date: '29/01/2000',
+      published: 'this is published',
+      attachment: 'attached',
+      notify: 'i have notify',
+      content: 'content',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/administrator/teamdocumentsharing/teamdocumentsharing.component.ts
+++ src/app/administrator/teamdocumentsharing/teamdocumentsharing.component.ts
@@ -3,13 +3,49 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-teamdocumentsharing',
   templateUrl: './teamdocumentsharing.component.html',
-  styleUrls: ['./teamdocumentsharing.component.css']
+  styleUrls: ['./teamdocumentsharing.component.css'],
 })
 export class TeamdocumentsharingComponent implements OnInit {
+  teamDocuments = [
+    {
+      title: 'Gilbert',
+      attachment: 'am on attachment',
+      category: 'my category',
+      description: 'this is description',
+    },
+    {
+      title: 'Gilbert',
+      attachment: 'am on attachment',
+      category: 'my category',
+      description: 'this is description',
+    },
+    {
+      title: 'Gilbert',
+      attachment: 'am on attachment',
+      category: 'my category',
+      description: 'this is description',
+    },
+    {
+      title: 'Gilbert',
+      attachment: 'am on attachment',
+      category: 'my category',
+      description: 'this is description',
+    },
+    {
+      title: 'Gilbert',
+      attachment: 'am on attachment',
+      category: 'my category',
+      description: 'this is description',
+    },
+    {
+      title: 'Gilbert',
+      attachment: 'am on attachment',
+      category: 'my category',
+      description: 'this is description',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
