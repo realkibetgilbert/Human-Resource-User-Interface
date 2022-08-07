c8fa1896dee2637074c07067ae255885ca34308a
=gilbertkibet <kibetgilly354@gmail.com>
2022-08-07 19:06:03 +0300

    doing login authentication

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { features } from 'process';
+import { FormGroup, FormControl } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { Router, Routes } from '@angular/router';
+import { AuthService } from '../shared/auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -9,13 +10,35 @@ import { Router, Routes } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
   hide = true;
+  email: string;
+  password: string;
+  formData: FormGroup;
 
-  constructor(private toastr: ToastrService, private router: Router) {}
+  constructor(
+    private toastr: ToastrService,
+    private router: Router,
+    private authService: AuthService
+  ) {}
 
-  ngOnInit(): void {}
-
-  logIn() {
-    this.toastr.success('Login Successfull');
-    this.router.navigate(['/administrator/home']);
+  ngOnInit(): void {
+    this.formData = new FormGroup({
+      email: new FormControl('kibetgilly354@gmail.com'),
+      password: new FormControl('123four'),
+    });
   }
+
+  onClickSubmitData(data: any) {
+    this.email = data.email;
+    this.password = data.password;
+
+    this.authService.login(this.email, this.password).subscribe((data) => {
+      console.log('Is log in success : ' + data);
+    });
+    if (data) this.router.navigate(['./administrator/home']);
+  }
+
+  // logIn() {
+  //   this.toastr.success('Login Successfull');
+  //   this.router.navigate(['/administrator/home']);
+  // }
 }
--- src/app/shared.service.ts
+++ src/app/shared.service.ts
@@ -2,7 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
-import { state } from '@angular/animations';
 @Injectable({
   providedIn: 'root',
 })
